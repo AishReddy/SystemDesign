4a24f64c331bc54175444f739d7e6971937548c4
Aishwarya Reddy <p.aishwaryareddy2511@gmail.com>
2026-05-11 02:06:16 +0530

    Initialize systemdesign package

---
+++ systemdesign/__init__.py
@@ -0,0 +1,12 @@
+"""
+SystemDesign Package
+A comprehensive resource for learning and implementing system design patterns.
+"""
+
+__version__ = "1.0.0"
+__author__ = "AishReddy"
+
+from . import basics
+from . import design
+
+__all__ = ['basics', 'design']
